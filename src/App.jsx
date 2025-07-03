b2cf9a37431e86cb045043f5725c094c29c7a7c3
Ashik TK <111106558+ashiktk85@users.noreply.github.com>
2025-07-04 01:51:09 +0200

    Initial commit: React + Tailwind + Material UI template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black">
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
